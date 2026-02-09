bd91775e516fe0ba95ac7a596bc03639c160a6e6
jerryyoon-qcells <jerry.yoon@qcells.com>
2026-02-09 07:37:51 -0800

    Initial commit from Specify template

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Keyboard Mouse Share - Main Application
+"""
+
+import speckit
+
+# Initialize speckit for chat prompt
+chat = speckit.Chat()
+
+if __name__ == "__main__":
+    print("Keyboard Mouse Share Application")
+    # Your chat prompt integration here
